e6d19270d1a190322ebf73ed73463f0dae35066b
Gustavo de Andrade <gustavo_641@live.com>
2019-10-18 16:08:23 -0300

    translated hosts.sh to english

--- hosts.sh
+++ hosts.sh
@@ -4,11 +4,11 @@ export $(egrep -v '^#' .env | xargs)
 
 if [ "`grep -c $PROJECT_NAME /etc/hosts`" -gt "0" ]
 then
-  echo "$PROJECT_NAME já existe em /etc/hosts"
+  echo "$PROJECT_NAME already exists in /etc/hosts"
 else
   printf "127.0.0.1\t$PROJECT_NAME.local\n" >> /etc/hosts
   printf "127.0.0.1\tphpmyadmin.$PROJECT_NAME.local" >> /etc/hosts
-  echo "$PROJECT_NAME adicionado com sucesso aos hosts"
+  echo "$PROJECT_NAME successfully added to hosts"
 fi
 
 exit 0
